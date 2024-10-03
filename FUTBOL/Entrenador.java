d4e87e05694afc98c3e71f66c51a251d7999491e
aalvaroo7 <146822468+aalvaroo7@users.noreply.github.com>
2024-10-03 18:02:07 +0200

    comit 1

--- FUTBOL/Entrenador.java
+++ FUTBOL/Entrenador.java
@@ -1,4 +1,16 @@
 package FUTBOL;
 
-public class Entrenador {
+public class Entrenador extends Personal {
+
+    public Entrenador(String id, String nombre, String apellidos, int edad) {
+        super(id, nombre, apellidos, edad);
+    }
+
+    public void dirigirPartido() {
+        System.out.println("Estoy dirigiendo el partido.");
+    }
+
+    public void dirigirEntrenamiento() {
+        System.out.println("Estoy dirigiendo un entrenamiento.");
+    }
 }
--- FUTBOL/Futbolista.java
+++ FUTBOL/Futbolista.java
@@ -1,4 +1,16 @@
 package FUTBOL;
 
-public class Futbolista {
+public class Futbolista extends Personal {
+
+    public Futbolista(String id, String nombre, String apellidos, int edad) {
+        super(id, nombre, apellidos, edad);
+    }
+
+    public void jugarPartido() {
+        System.out.println("Estoy jugando el partido.");
+    }
+
+    public void entrenar() {
+        System.out.println("Estoy entrenando.");
+    }
 }
