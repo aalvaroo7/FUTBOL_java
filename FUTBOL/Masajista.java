f1c2862955a1c7f19b94d9e0bfa082952989d999
aalvaroo7 <146822468+aalvaroo7@users.noreply.github.com>
2024-10-03 18:03:10 +0200

    comit 2

--- FUTBOL/Masajista.java
+++ FUTBOL/Masajista.java
@@ -1,4 +1,12 @@
 package FUTBOL;
 
-public class Masajista {
+public class Masajista extends Personal {
+
+    public Masajista(String id, String nombre, String apellidos, int edad) {
+        super(id, nombre, apellidos, edad);
+    }
+
+    public void darMasaje() {
+        System.out.println("Estoy dando un masaje.");
+    }
 }
