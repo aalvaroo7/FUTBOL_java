bc088e8b7ec1a3428ec73e745de40c24fe2db484
aalvaroo7 <146822468+aalvaroo7@users.noreply.github.com>
2024-10-03 18:00:09 +0200

    comit inicial

--- FUTBOL/Personal.java
+++ FUTBOL/Personal.java
@@ -1,4 +1,23 @@
 package FUTBOL;
 
 public class Personal {
+    private String id;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Personal(String id, String nombre, String apellidos, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public void concentrarse() {
+        System.out.println(nombre + " se está concentrando.");
+    }
+
+    public void viajar() {
+        System.out.println(nombre + " está viajando.");
+    }
 }
